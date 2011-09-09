c330108a73fc7cb4b07b39454000e4f09aefb4f7
Erik Amaru Ortiz <erik@colosa.com>
2011-09-09 10:52:33 -0400

    added front end to create problems and upload its input & output file

--- devcon/controllers/problems.py
+++ devcon/controllers/problems.py
@@ -38,6 +38,8 @@ from sqlalchemy.sql.expression import or_
 from tg import tmpl_context
 from devcon.widgets.movie_form import create_movie_form
 from devcon.widgets.problem_submit import create_submit_form
+from devcon.widgets.problem_new import create_new_problem
+from devcon.widgets.problem_upload_io import create_upload_io_form
 from devcon import model
 from devcon.model import DBSession, metadata, Problems, Submits, Series, Results
 
@@ -48,6 +50,14 @@ problems_grid = DataGrid(fields=[
     ('Date', 'date'),
     ('Topic', 'topic'),
     ('', lambda obj:genshi.Markup('<a href="%s">view</a>' % url('/problems/view', params=dict(uid=obj.uid))))
+])
+
+problems_grid_adm = DataGrid(fields=[
+    ('Problem', 'code'),
+    ('Title', 'title'),
+    ('Date', 'date'),
+    ('Topic', 'topic'),
+    ('', lambda obj:genshi.Markup('<a href="%s">Upload Input/Output</a>' % url('/problems/upload_io_files', params=dict(uid=obj.uid))))
 ])
 
 submits_grid = DataGrid(fields=[
@@ -125,28 +135,47 @@ class ProblemsController(BaseController):
     #@validate(create_submit_form, error_handler=submit)
     def save(self, **kw):
         if kw['file_source'] != '':
+            # getting problem data
             problem = DBSession.query(Problems).filter_by(uid=kw['uid']).one()
+
+            # getting attempt number
+            q =  DBSession.query(Submits).filter(Submits.user_id==request.identity['user'].user_id). \
+                 filter(Submits.problem_id==problem.uid).order_by(desc(Submits.attempt))
+            if q.count() > 0:
+                attempt_nro = q[0].attempt + 1
+            else:
+                attempt_nro = 1
+
+            # getting the current serie of contest
+            serie = DBSession.query(Series).filter_by(current=1)
+            try:
+                serie_num = serie.one().uid
+                title = serie.one().title
+            except:
+                serie_num = 0
+                title = 'There is not a active contest'
+
             
             """ saving the file """
             file = request.POST['file_source']
             asset_dirname = os.path.join(os.getcwd(), 'devcon/public/files')
             
             #real path to store the file by user
-            path = os.path.join(asset_dirname, request.identity['user'].user_name)
-            #is dir??
-            if not os.path.isdir(path):
-                os.mkdir(path)
+            user_dir = os.path.join(asset_dirname, request.identity['user'].user_name)
+            user_serie_dir = os.path.join(user_dir, 'serie_' + str(serie_num))
             
-            submit_filename = "%s_%d" % (file.filename.lstrip(os.sep), problem.serie)
-            filepath = os.path.join(path, submit_filename)
+            #is dir??
+            if not os.path.isdir(user_dir):
+                os.mkdir(user_dir)
+            if not os.path.isdir(user_serie_dir):
+                os.mkdir(user_serie_dir)
+            
+            submit_filename = "%s_%d_%d.php" % (file.filename.lstrip(os.sep).replace('.php', ''), problem.serie, attempt_nro)
+            filepath = os.path.join(user_serie_dir, submit_filename)
             
             permanent_file = open(filepath, 'w')
             shutil.copyfileobj(file.file, permanent_file)
-            
-            filename = file.filename
-            filesize = os.path.getsize(os.path.join(path, file.filename.lstrip(os.sep)))
-            filesize = filesize / 1024
-            filesize = "%d Kb" % (filesize, )
+            filesize = "%d Kb" % ((os.path.getsize(filepath)/1024))
             
             file.file.close()
             #this_file = self.request.params["file"].filename 
@@ -155,51 +184,48 @@ class ProblemsController(BaseController):
 
             """ saving on db """
             
-            q =  DBSession.query(Submits).filter(Submits.user_id==request.identity['user'].user_id).filter(Submits.problem_id==problem.uid).order_by(desc(Submits.attempt))
-            
-            if q.count() > 0:
-                attempt_nro = q[0].attempt + 1
-            else:
-                attempt_nro = 1
-            
             filename = "%s_%d" % (problem.code.lower(), problem.serie)
             inputfile_path = os.path.join(os.getcwd(), 'devcon/public/files/inputs', filename + '.in')
             output_filename = filename + '.out' 
             gen_output_filename = "%s_%d.out" % (filename, attempt_nro) 
             outputfile_path = os.path.join(os.getcwd(), 'devcon/public/files/outputs', output_filename)
-            tmp_filepath = os.path.join(path, gen_output_filename)
+            tmp_filepath = os.path.join(user_serie_dir, gen_output_filename)
             
             cmd = "%s < %s > %s" % (filepath, inputfile_path, tmp_filepath)
 
-            os.system(cmd)
+            excution_res = os.system(cmd)
             os.chmod(tmp_filepath, stat.S_IRWXU)
-            
-            # verify if the file was generated
-            if os.path.isfile(tmp_filepath):
-                test_lines = open(tmp_filepath).readlines()
-                correct_lines = open(outputfile_path).readlines()
-                
-                failed = 0
-                len_diff = len(test_lines) - len(correct_lines)
 
-                if len_diff <> 0:
-                    result = 'wrong answer'
-                    failed = 1
-                else:
-                    for test, correct in zip(test_lines, correct_lines):
-                        if test != correct:
-                            result = 'wrong answer'
-                            failed = 1
-                            break
-                
-                if failed == 0:
-                    ok = 1
+            # verify if the file was generated
+            if excution_res == 0:
+                if os.path.isfile(tmp_filepath):
+                    test_lines = open(tmp_filepath).readlines()
+                    correct_lines = open(outputfile_path).readlines()
+                    
+                    failed = 0
+                    len_diff = len(test_lines) - len(correct_lines)
+
+                    if len_diff != 0:
+                        result = 'wrong answer'
+                        failed = 1
+                    else:
+                        for test, correct in zip(test_lines, correct_lines):
+                            if test != correct:
+                                result = 'wrong answer'
+                                failed = 1
+                                break
+                    
+                    if failed == 0:
+                        ok = 1
+                    else:
+                        ok = 0
                 else:
                     ok = 0
+                    result = 'compilation output missing'
 
             else:
                 ok = 0
-                result = 'compilation failed'
+                result = 'compilation error'
                 
             if ok == 1:
                 result = 'accepted'
@@ -215,7 +241,7 @@ class ProblemsController(BaseController):
             _submit.output_filename = gen_output_filename
             _submit.attempt = attempt_nro
             _submit.result = result
-            _submit.comments = kw['comments']
+            _submit.comments = kw['Comments']
             _submit.accepted = ok
             DBSession.add(_submit)
             
@@ -261,3 +287,97 @@ while($a != 0) {
 """
         return dict(page='gs', scode=scode)
 
+    @expose('devcon.templates.problems.new_form')
+    def new(self, **kw):
+        tmpl_context.form = create_new_problem
+        return dict(value=kw)
+
+    @expose('devcon.templates.problems.create_new')
+    def createNew(self, **kw):
+        problem = Problems()
+        problem.code = kw['code']
+        problem.title = kw['title']
+        problem.text = kw['text']
+        problem.input = kw['input']
+        problem.output = kw['output']
+        problem.sample_input = kw['sample_input']
+        problem.sample_ouput = kw['sample_output']
+        problem.topic = kw['topic']
+        problem.serie = kw['serie']
+        problem.points = kw['points']
+        problem.lang = 'en'
+        problem.date = datetime.datetime.today().isoformat()
+
+        DBSession.add(problem)
+
+        flash(_('Problem Saved for serie: ' + str(kw['serie'])))
+        redirect('/problems/list_adm')
+
+    @expose('devcon.templates.problems.list_adm')
+    def list_adm(self):
+        # getting the current serie of contest
+        serie = DBSession.query(Series).filter_by(current=1)
+        
+        try:
+            uid = serie.one().uid
+            title = serie.one().title
+        except:
+            uid = 0
+            title = 'There is not a active contest'
+        
+        data = DBSession.query(Problems). \
+               filter_by(serie=uid). \
+               order_by(Problems.uid)
+        
+        return dict(page='list', grid=problems_grid_adm, data=data, request=request, title=title)
+
+    
+    @expose('devcon.templates.problems.upload_io_form')
+    def upload_io_files(self, **kw):
+        tmpl_context.form = create_upload_io_form
+        return dict(value=kw)
+
+    @expose()
+    def upload_io_save(self, uid, **kw):
+        # getting problem data
+        problem = DBSession.query(Problems).filter_by(uid=uid).one()
+
+        # getting the current serie of contest
+        serie = DBSession.query(Series).filter_by(current=1)
+        try:
+            serie_num = serie.one().uid
+        except:
+            serie_num = 0
+
+        """ saving the file """
+        in_file = request.POST['in_file']
+        out_file = request.POST['out_file']
+        in_name = "%s_%s.in" % (problem.code.lower(), serie_num)
+        out_name = "%s_%s.out" % (problem.code.lower(), serie_num)
+
+        input_path = os.path.join(os.getcwd(), 'devcon/public/files/inputs')
+        output_path = os.path.join(os.getcwd(), 'devcon/public/files/outputs')
+       
+        #is dir??
+        if not os.path.isdir(input_path):
+            os.mkdir(input_path)
+        if not os.path.isdir(output_path):
+            os.mkdir(output_path)
+
+        input_path = os.path.join(input_path, in_name)
+        output_path = os.path.join(output_path, out_name)
+        
+        permanent_in_file = open(input_path, 'w')
+        permanent_out_file = open(output_path, 'w')
+
+        shutil.copyfileobj(in_file.file, permanent_in_file)
+        shutil.copyfileobj(out_file.file, permanent_out_file)
+        
+        in_file.file.close()
+        out_file.file.close()
+        
+        os.chmod(input_path, stat.S_IRWXU)
+        os.chmod(output_path, stat.S_IRWXU)
+
+        flash(_('Files uploaded: ' + input_path + '\n' + output_path))
+        redirect('/problems/list_adm')
